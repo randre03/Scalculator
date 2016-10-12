ea60d643bdf57692578f832152513e38d13a695d
Roger Randall <roger@rogerrandall.com>
2016-10-12 00:07:06 -0400

    Initial Commit

---
+++ src/main/scala/calculator/Polynomial.scala
@@ -0,0 +1,30 @@
+package calculator
+
+object Polynomial {
+
+  def computeDelta(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double]): Signal[Double] = {
+
+    Signal{
+      val bp = b()
+      (bp*bp)-(4*a()*c())
+    }
+  }
+
+  def computeSolutions(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+
+    val negB = Signal(-1 * b())
+    val twoA = Signal(2 * a())
+    val sqrtDelta = Signal(scala.math.sqrt(delta()))
+
+    Signal {
+      if(delta() < 0) Set()
+      else {
+        Set(
+          (negB() + sqrtDelta()) / twoA(),
+          (negB() - sqrtDelta()) / twoA())
+      }
+    }
+  }
+}
